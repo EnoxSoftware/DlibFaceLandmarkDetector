702eab8bafbb6f9023bec38da5e669965a4ea754
satoo air <satoo>
2016-11-15 17:26:09 +0900

    update 1.0.3

--- doc/html/search/functions_2.js
+++ doc/html/search/functions_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['getfilepath',['getFilePath',['../class_dlib_face_landmark_detector_1_1_utils.html#adb54b69773f3360e61e7af1a90ded381',1,'DlibFaceLandmarkDetector::Utils']]],
+  ['getfilepath',['getFilePath',['../class_dlib_face_landmark_detector_1_1_utils.html#a61f02890aa996062d9ba0978de5d5792',1,'DlibFaceLandmarkDetector::Utils']]],
+  ['getfilepathasync',['getFilePathAsync',['../class_dlib_face_landmark_detector_1_1_utils.html#aba908ee646c11ad006bb99e8a87306e1',1,'DlibFaceLandmarkDetector::Utils']]],
   ['getshapepredictornumfeatures',['GetShapePredictorNumFeatures',['../class_dlib_face_landmark_detector_1_1_face_landmark_detector.html#a7f80df65d948942d62e833b571038f49',1,'DlibFaceLandmarkDetector::FaceLandmarkDetector']]],
   ['getshapepredictornumparts',['GetShapePredictorNumParts',['../class_dlib_face_landmark_detector_1_1_face_landmark_detector.html#a9facb7083b01fcb99a07a13042135255',1,'DlibFaceLandmarkDetector::FaceLandmarkDetector']]]
 ];

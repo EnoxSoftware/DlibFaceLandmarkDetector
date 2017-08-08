1c99c35e3c7cd603620743f66be4e7fa865630c8
EnoxSoftware <enox.software@gmail.com>
2017-08-08 13:00:01 +0900

    update 1.1.4

--- doc/html/search/functions_2.js
+++ doc/html/search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['getfilepath',['getFilePath',['../class_dlib_face_landmark_detector_1_1_utils.html#a61f02890aa996062d9ba0978de5d5792',1,'DlibFaceLandmarkDetector::Utils']]],
-  ['getfilepathasync',['getFilePathAsync',['../class_dlib_face_landmark_detector_1_1_utils.html#a9920e15bb0e0c895f898a6681110e590',1,'DlibFaceLandmarkDetector::Utils']]],
+  ['getfilepathasync',['getFilePathAsync',['../class_dlib_face_landmark_detector_1_1_utils.html#afde6b3994795b6ba42e93e54bbffb639',1,'DlibFaceLandmarkDetector::Utils']]],
   ['getshapepredictornumfeatures',['GetShapePredictorNumFeatures',['../class_dlib_face_landmark_detector_1_1_face_landmark_detector.html#a7f80df65d948942d62e833b571038f49',1,'DlibFaceLandmarkDetector::FaceLandmarkDetector']]],
   ['getshapepredictornumparts',['GetShapePredictorNumParts',['../class_dlib_face_landmark_detector_1_1_face_landmark_detector.html#a9facb7083b01fcb99a07a13042135255',1,'DlibFaceLandmarkDetector::FaceLandmarkDetector']]],
   ['getversion',['getVersion',['../class_dlib_face_landmark_detector_1_1_utils.html#ab5936fd78d8a91f8eb2a4a1e7030f16e',1,'DlibFaceLandmarkDetector::Utils']]]
